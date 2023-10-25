295fe4badec35e63d8446811fbb42de469a51465
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-10-25 20:08:04 -0300

    🐎: Alert browser on SSE event

---
+++ frontend/src/sse.js
@@ -0,0 +1,36 @@
+if (!!window.EventSource) {
+  console.log("Init");
+
+  var source = new EventSource("/sse");
+
+  source.addEventListener(
+    "message",
+    function (e) {
+      alert(e.data);
+    },
+    false
+  );
+
+  source.addEventListener(
+    "open",
+    function (e) {
+      console.log("Connected");
+    },
+    false
+  );
+
+  source.addEventListener(
+    "error",
+    function (e) {
+      if (e.eventPhase == EventSource.CLOSED) source.close();
+      if (e.target.readyState == EventSource.CLOSED) {
+        console.error("Disconnected");
+      } else if (e.target.readyState == EventSource.CONNECTING) {
+        console.error("Connecting...");
+      }
+    },
+    false
+  );
+} else {
+  console.log("Your browser doesn't support SSE");
+}
